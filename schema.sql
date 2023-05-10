509ff103b0c8c50bed0e7c4f1309b66f58e2c245
jhonV <jhonv.v1@gmail.com>
2023-05-10 03:50:21 -0500

    configuracion de d1

---
+++ schema.sql
@@ -0,0 +1,3 @@
+DROP TABLE IF EXISTS Users;
+CREATE TABLE Users (Email TEXT, UserPassword TEXT, UserName TEXT, Age TEXT, UserRole TEXT);
+INSERT INTO Users (Email, UserPassword, UserName,Age,UserRole) VALUES ('jhonv.v1@gmail.com','12345','jhonv','30','Admin');
--- src/index.ts
+++ src/index.ts
@@ -9,25 +9,24 @@
  */
 
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-}
-
-export default {
-	async fetch(
-		request: Request,
-		env: Env,
-		ctx: ExecutionContext
-	): Promise<Response> {
-		return new Response("Hello World!");
+	harnonDb: D1Database;
+  }
+  
+  export default {
+	async fetch(request: Request, env: Env) {
+	  const { pathname } = new URL(request.url);
+  
+	  if (pathname === "/api/users") {
+		const { results } = await env.harnonDb.prepare(
+		  "SELECT * FROM Users WHERE CompanyName = ?"
+		)
+		  .bind("Bs Beverages")
+		  .all();
+		return Response.json(results);
+	  }
+  
+	  return new Response(
+		"Call /api/beverages to see everyone who works at Bs Beverages"
+	  );
 	},
-};
+  };
