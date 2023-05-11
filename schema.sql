d4c45fa126729444f149f0201c083defc3698146
jhonV <jhonv.v1@gmail.com>
2023-05-10 22:33:19 -0500

    correccion password

--- schema.sql
+++ schema.sql
@@ -1,3 +1,3 @@
 DROP TABLE IF EXISTS Users;
 CREATE TABLE Users (email TEXT, userPassword TEXT, userName TEXT, age TEXT, userRole TEXT);
-INSERT INTO Users (email, userPassword, userName, age, userRole) VALUES ('jhonv.v1@gmail.com','12345','jhonv','30','Admin');
+INSERT INTO Users (email, userPassword, userName, age, userRole) VALUES ('jhonv.v1@gmail.com','12345Hi*','jhonv','30','Admin');
